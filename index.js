5397d8320c65326f9b38b49e504e6c0579345a0a
shannon code <shannoncode@shannons-MacBook-Pro-2.local>
2018-04-11 17:08:09 -0400

    cryptokitty and ethereum support

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var Coval = require('coval.js')
 var MultiSign = require('./lib/address.js')
+var cryptokittiesContrib = require('cryptokitties-contrib');
+var ck = new cryptokittiesContrib();
+window.ck = ck
 window.Coval = Coval
 window.MultiSign = MultiSign
 window.Buffer = function(input){
