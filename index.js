0304f2093311dfb7d57b9b157d004b7e5cd44001
shannon code <shannoncode@shannons-MBP-2.lan>
2018-08-02 19:47:49 -0400

    btcp

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-//var Coval = require('coval.js')
+var Coval = require('coval.js')
 var MultiSign = require('./lib/address.js')
 var cryptokittiesContrib = require('cryptokitties-contrib');
 var ck = new cryptokittiesContrib();
 window.ck = ck
-//window.Coval = Coval
+window.Coval = Coval
 window.MultiSign = MultiSign
 window.Buffer = function(input){
     return new Buffer(input)
